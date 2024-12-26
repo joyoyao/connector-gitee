e55d377930937fa86f6d91b2285998bd36032e25
joyoyao <zaiyongs@gmail.com>
2024-12-26 23:02:48 +0800

    gitee 连接器

---
+++ i18n/translation.go
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package i18n
+
+const (
+	ConnectorName                            = "plugin.gitee_connector.backend.name"
+	InfoName                                 = "plugin.gitee_connector.backend.info.name"
+	InfoDescription                          = "plugin.gitee_connector.backend.info.description"
+	ConfigNameTitle                          = "plugin.gitee_connector.backend.config.name.title"
+	ConfigNameDescription                    = "plugin.gitee_connector.backend.config.name.description"
+	ConfigClientIDTitle                      = "plugin.gitee_connector.backend.config.client_id.title"
+	ConfigClientIDDescription                = "plugin.gitee_connector.backend.config.client_id.description"
+	ConfigClientSecretTitle                  = "plugin.gitee_connector.backend.config.client_secret.title"
+	ConfigClientSecretDescription            = "plugin.gitee_connector.backend.config.client_secret.description"
+	ConfigAuthorizeUrlTitle                  = "plugin.gitee_connector.backend.config.authorize_url.title"
+	ConfigAuthorizeUrlDescription            = "plugin.gitee_connector.backend.config.authorize_url.description"
+	ConfigTokenUrlTitle                      = "plugin.gitee_connector.backend.config.token_url.title"
+	ConfigTokenUrlDescription                = "plugin.gitee_connector.backend.config.token_url.description"
+	ConfigUserJsonUrlTitle                   = "plugin.gitee_connector.backend.config.user_json_url.title"
+	ConfigUserJsonUrlDescription             = "plugin.gitee_connector.backend.config.user_json_url.description"
+	ConfigUserIDJsonPathTitle                = "plugin.gitee_connector.backend.config.user_id_json_path.title"
+	ConfigUserIDJsonPathDescription          = "plugin.gitee_connector.backend.config.user_id_json_path.description"
+	ConfigUserDisplayNameJsonPathTitle       = "plugin.gitee_connector.backend.config.user_display_name_json_path.title"
+	ConfigUserDisplayNameJsonPathDescription = "plugin.gitee_connector.backend.config.user_display_name_json_path.description"
+	ConfigUserUsernameJsonPathTitle          = "plugin.gitee_connector.backend.config.user_username_json_path.title"
+	ConfigUserUsernameJsonPathDescription    = "plugin.gitee_connector.backend.config.user_username_json_path.description"
+	ConfigUserEmailJsonPathTitle             = "plugin.gitee_connector.backend.config.user_email_json_path.title"
+	ConfigUserEmailJsonPathDescription       = "plugin.gitee_connector.backend.config.user_email_json_path.description"
+	ConfigUserAvatarJsonPathTitle            = "plugin.gitee_connector.backend.config.user_avatar_json_path.title"
+	ConfigUserAvatarJsonPathDescription      = "plugin.gitee_connector.backend.config.user_avatar_json_path.description"
+	ConfigCheckEmailVerifiedTitle            = "plugin.gitee_connector.backend.config.check_email_verified.title"
+	ConfigCheckEmailVerifiedLabel            = "plugin.gitee_connector.backend.config.check_email_verified.label"
+	ConfigEmailVerifiedJsonPathTitle         = "plugin.gitee_connector.backend.config.email_verified_json_path.title"
+	ConfigEmailVerifiedJsonPathDescription   = "plugin.gitee_connector.backend.config.email_verified_json_path.description"
+	ConfigScopeTitle                         = "plugin.gitee_connector.backend.config.scope.title"
+	ConfigScopeDescription                   = "plugin.gitee_connector.backend.config.scope.description"
+	ConfigLogoSVGTitle                       = "plugin.gitee_connector.backend.config.logo_svg.title"
+	ConfigLogoSVGDescription                 = "plugin.gitee_connector.backend.config.logo_svg.description"
+)
